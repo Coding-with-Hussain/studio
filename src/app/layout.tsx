f3935bcdb33338a053e6340bddbf3ded3de5ef73
Muhmmad Hussain <hussainmuhmmad152@gmail.com>
2025-04-28 20:25:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./src/app/layout.tsx
    > Module not found: Can't resolve 'next/font/google/target.css'
    > Unknown font
    >
    > Debug info:
    > - Execution of <ModuleAssetContext as AssetContext>::resolve_asset failed
    > - Execution of resolve failed
    > - Execution of resolve_internal failed
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    > Error while looking up import map: Unknown font
    >
    > Debug info:
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    > . Error source: Call Stack
    > 7
    >
    > Show 7 ignore-listed frame(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from 'next';
-import { Geist_Sans as Geist, Geist_Mono } from 'next/font/google';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import './globals.css';
 import { ThemeProvider } from "@/components/theme-provider"; // Import ThemeProvider
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'RateShift Currency Converter',
@@ -26,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
         <ThemeProvider
